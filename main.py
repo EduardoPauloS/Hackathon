2755a0e2401e46790e3694a6dfe830d9d497976d
Paulo Eduardo Seraphim da Silva <Paulo.Eduardo.Silva@ibm.com>
2022-10-14 18:20:46 -0300

    -BACK
    - python version removed

--- main.py
+++
@@ -1,6 +0,0 @@
-from searchtweets import load_credentials
-
-load_credentials(filename="config.yaml",
-                 yaml_key=search_tweets_v2,
-                 env_overwrite=True,
-                )
